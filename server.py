ff4cd4fbc873359922acc7565d479cea5118e198
Marcelo Henrique <153032141+marceloHenriquePS@users.noreply.github.com>
2026-06-13 19:34:40 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,40 +2,167 @@ import socket, threading
 
 class ServerMessages:
     SERVER_RUNNING = "Server running on {HOST}:{PORT}.\nWaiting for connections..."
-    USER_CONNECTED = "[+] {name} connected."
-    USER_DISCONNECTED = "[-] {name} disconnected."
-    USER_JOIN_CHAT = "[Server] {name} joined the chat!"
-    USER_LEFT_CHAT = "[Server] {name} left the chat."
-    NICKNAME_ALREADY_EXISTS = "[Server] Nickname already in use!"
+
+    USER_CONNECTED = "[Server] {user} connected."
+    USER_DISCONNECTED = "[Server] {user} disconnected."
+    USER_JOIN_CHAT = "[Server] {user} joined the chat!"
+    USER_LEFT_CHAT = "[Server] {user} left the chat."
+    USER_JOIN_GROUP = "[Server] {user} joined the {group} group!"
+    USER_LEFT_GROUP = "[Server] {user} left the {group} group."
+    USER_EXIST_IN_GROUP = "[Server] User is already part of this group."
+    USER_NOT_EXIST_IN_GROUP = "[Server] User is not part of this group."
+    ERROR_USER_EXISTS = "[Server] Nickname already in use!"
+
+    GROUP_EXISTS = "[Server] Group already exists."
+    GROUP_NOT_EXISTS = "[Server] Group not exists."
+    GROUP_CREATED = "[Server] Group '{group}' created successfully."
+
+    ERROR_COMMAND_HAS_NOT_ARGUMENTS = "[Server] Command '{command}' does not accept arguments."
+    ERROR_COMMAND_EXPECTS_ARGUMENTS = "[Server] Command '{command}' expects arguments."
 
 class ServerCommands:
     EXIT = "-exit"
-    LIST_ALL_USERS = "-listAllUsers"
+    LIST_USERS = "-listusers"
+    LIST_GROUPS = "-listgroups"
+    LIST_USERS_IN_GROUP = "-listgroupU"
+    CREATE_GROUP = "-creategroup"
+    JOIN_GROUP = "-joingroup"
+    LEAVE_GROUP = "-leavegroup"
 
-def exitCommand(client):
+def exitCommand(client, message):
+    if commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_HAS_NOT_ARGUMENTS.format(command=ServerCommands.EXIT), client)
+        return
+    
     removeClient(client)
+    return
 
-def listAllUsers(client):
+def listAllUsers(client, message):
+    if commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_HAS_NOT_ARGUMENTS.format(command=ServerCommands.LIST_USERS), client)
+        return
+
     users = []
 
     for name in clients.values():
         users.append(name)
 
-    message = f"Online: {users}"
+    listUsers = f"Online: {users}"
 
-    sendToClient(encodeMessage(message), client)
+    sendToClient(listUsers, client)
+    return
+
+def createGroup(client, message):
+    if not commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_EXPECTS_ARGUMENTS.format(command=ServerCommands.CREATE_GROUP), client)
+        return
+    
+    groupName = message.replace(ServerCommands.CREATE_GROUP, "", 1).strip()
+
+    if groupName in groups:
+        sendToClient(ServerMessages.GROUP_EXISTS, client)
+        return
+
+    groups[groupName] = set()
+    sendToClient(ServerMessages.GROUP_CREATED.format(group=groupName), client)
+    return
+
+def joinGroup(client, message):
+    if not commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_EXPECTS_ARGUMENTS.format(command=ServerCommands.JOIN_GROUP), client)
+        return
+    
+    groupName = message.replace(ServerCommands.JOIN_GROUP, "", 1).strip()
+
+    if groupName not in groups:
+        sendToClient(ServerMessages.GROUP_NOT_EXISTS, client)
+        return
+    
+    user = clients[client]
+
+    if user in groups[groupName]:
+        sendToClient(ServerMessages.USER_EXIST_IN_GROUP, client)
+        return
+    
+    groups[groupName].add(clients[client])
+    sendToClient(ServerMessages.USER_JOIN_GROUP.format(user=user, group=groupName), client)
+    return
+
+def leaveGroup(client, message):
+    if not commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_EXPECTS_ARGUMENTS.format(command=ServerCommands.LEAVE_GROUP), client)
+        return
+    
+    groupName = message.replace(ServerCommands.LEAVE_GROUP, "", 1).strip()
+
+    if groupName not in groups:
+        sendToClient(ServerMessages.GROUP_NOT_EXISTS, client)
+        return
+    
+    user = clients[client]
+
+    if user not in groups[groupName]:
+        sendToClient(ServerMessages.USER_NOT_EXIST_IN_GROUP, client)
+        return
+    
+    groups[groupName].remove(clients[client])
+    sendToClient(ServerMessages.USER_LEFT_GROUP.format(user=user, group=groupName), client)
+    return
+
+def listAllGroups(client, message):
+    if commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_HAS_NOT_ARGUMENTS.format(command=ServerCommands.LIST_GROUPS), client)
+        return
+
+    groupsCurrent = []
+
+    for groupName in groups.keys():
+        groupsCurrent.append(groupName)
+
+    listGroups = f"Groups: {groupsCurrent}"
+
+    sendToClient(listGroups, client)
+    return
+
+def listAllUsersInGroup(client, message):
+    if not commandHasArguments(message):
+        sendToClient(ServerMessages.ERROR_COMMAND_EXPECTS_ARGUMENTS.format(command=ServerCommands.LIST_USERS_IN_GROUP), client)
+        return
+    
+    groupName = message.replace(ServerCommands.LIST_USERS_IN_GROUP, "", 1).strip()
+
+    if groupName not in groups:
+        sendToClient(ServerMessages.GROUP_NOT_EXISTS, client)
+        return
+    
+    groupUsers = []
+    for users in groups[groupName]:
+        groupUsers.append(users)
+    
+    listGroupsUsers = f"Group Users: {groupUsers}"
+
+    sendToClient(listGroupsUsers, client)
+    return
 
 HOST = '127.0.0.1'
 PORT = 55555
 
 clients = {}
+groups = {}
+
 commands = {
     ServerCommands.EXIT: exitCommand,
-    ServerCommands.LIST_ALL_USERS: listAllUsers
-            }
+    ServerCommands.LIST_USERS: listAllUsers,
+    ServerCommands.LIST_GROUPS: listAllGroups,
+    ServerCommands.LIST_USERS_IN_GROUP: listAllUsersInGroup,
+    ServerCommands.CREATE_GROUP: createGroup,
+    ServerCommands.JOIN_GROUP: joinGroup,
+    ServerCommands.LEAVE_GROUP: leaveGroup
+    }
 
 def sendToClient(message, client):
-    client.send(message)
+    encodedMessage = encodeMessage(message)
+    client.send(encodedMessage)
 
 def sendToAll(message, senderClient):
     for client in clients:
@@ -63,21 +190,21 @@ def receiveMessage(client):
         return None
 
 def clientService(client):
-    name = receiveMessage(client)
+    user = receiveMessage(client)
 
-    if not name:
+    if not user:
         removeClient(client)
         return
 
-    if nickNameExists(name):
-        sendToClient(encodeMessage(ServerMessages.NICKNAME_ALREADY_EXISTS), client)
+    if nickNameExists(user):
+        sendToClient(ServerMessages.ERROR_USER_EXISTS, client)
         client.close()
         return
 
-    clients[client] = name
+    clients[client] = user
 
-    print(ServerMessages.USER_CONNECTED.format(name=name))
-    sendToAll(encodeMessage(ServerMessages.USER_JOIN_CHAT.format(name=name)), client)
+    print(ServerMessages.USER_CONNECTED.format(user=user))
+    sendToAll(ServerMessages.USER_JOIN_CHAT.format(user=user), client)
 
     while True:
         message = receiveMessage(client)
@@ -85,21 +212,36 @@ def clientService(client):
         if not message:
             break
 
-        if message in commands:
-            commands[message](client)
+        command = getCommand(message)
+
+        if command in commands:
+            commands[command](client, message)
             continue
 
-        messageFormatted = f"{name}: {message}"
+        messageFormatted = f"{user}: {message}"
         print(messageFormatted)
-        sendToAll(encodeMessage(messageFormatted), client)
+        sendToAll(messageFormatted, client)
 
     removeClient(client)
-    print(ServerMessages.USER_DISCONNECTED.format(name=name))
-    sendToAll(encodeMessage(ServerMessages.USER_LEFT_CHAT.format(name=name)), client)
+    print(ServerMessages.USER_DISCONNECTED.format(user=user))
+    sendToAll(ServerMessages.USER_LEFT_CHAT.format(user=user), client)
 
 def nickNameExists(name):
     if name in clients.values():
         return True
+
+def getCommand(message):
+    parts = message.split()
+    command = parts[0]
+    return command
+
+def commandHasArguments(message):
+    parts = message.split()
+
+    if len(parts) != 1:
+        return True
+    
+    return False
 
 def encodeMessage(message):
     return message.encode('utf-8')
